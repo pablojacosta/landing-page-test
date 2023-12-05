67b7eeed99bb8d3289a0571c9c2ac8e2699ef24a
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-12-05 15:51:43 -0300

    refactor: :recycle: Refactored cards.

--- src/App.tsx
+++ src/App.tsx
@@ -132,73 +132,48 @@ function App() {
 
           <div className="row">
             <div className="col-1-of-3">
-              <Card />
+              <Card
+                title="The Sea Explorer"
+                details={{
+                  days: "3",
+                  people: "30",
+                  guides: "2",
+                  sleep: "cozy hotels",
+                  difficulty: "easy",
+                }}
+                buttonText="Book now!"
+                price="297"
+              />
             </div>
 
             <div className="col-1-of-3">
-              <div className="card">
-                <div className="card__side card__side--front">
-                  <div className="card__picture card__picture--2">&nbsp;</div>
-                  <h4 className="card__heading">
-                    <span className="card__heading-span card__heading-span--2">
-                      The Forest Hiker
-                    </span>
-                  </h4>
-                  <div className="card__details">
-                    <ul>
-                      <li>7 day tours</li>
-                      <li>Up to 40 people</li>
-                      <li>6 tour guides</li>
-                      <li>Sleep in provided tents</li>
-                      <li>Difficulty: medium</li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="card__side card__side--back card__side--back-2">
-                  <div className="card__cta">
-                    <div className="card__price-box">
-                      <p className="card__price-only">Only</p>
-                      <p className="card__price-value">$497</p>
-                    </div>
-                    <a href="#popup" className="btn btn--white">
-                      Book now!
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <Card
+                title="The Forest Hiker"
+                details={{
+                  days: "7",
+                  people: "40",
+                  guides: "6",
+                  sleep: "provided tents",
+                  difficulty: "medium",
+                }}
+                buttonText="Book now!"
+                price="497"
+              />
             </div>
 
             <div className="col-1-of-3">
-              <div className="card">
-                <div className="card__side card__side--front">
-                  <div className="card__picture card__picture--3">&nbsp;</div>
-                  <h4 className="card__heading">
-                    <span className="card__heading-span card__heading-span--3">
-                      The Snow Adventurer
-                    </span>
-                  </h4>
-                  <div className="card__details">
-                    <ul>
-                      <li>5 day tours</li>
-                      <li>Up to 15 people</li>
-                      <li>3 tour guides</li>
-                      <li>Sleep in provided tents</li>
-                      <li>Difficulty: hard</li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="card__side card__side--back card__side--back-3">
-                  <div className="card__cta">
-                    <div className="card__price-box">
-                      <p className="card__price-only">Only</p>
-                      <p className="card__price-value">$897</p>
-                    </div>
-                    <a href="#popup" className="btn btn--white">
-                      Book now!
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <Card
+                title="The Snow Adventurer"
+                details={{
+                  days: "5",
+                  people: "15",
+                  guides: "3",
+                  sleep: "provided tents",
+                  difficulty: "hard",
+                }}
+                buttonText="Book now!"
+                price="897"
+              />
             </div>
           </div>
 
--- src/components/elements/Card/Card.tsx
+++ src/components/elements/Card/Card.tsx
@@ -1,6 +1,21 @@
 import styles from "./Card.module.scss";
 
-const Card = () => {
+interface ICard {
+  title: string;
+  details: {
+    days: string;
+    people: string;
+    guides: string;
+    sleep: string;
+    difficulty: string;
+  };
+  price: string;
+  buttonText: string;
+}
+
+const Card = ({ title, details, price, buttonText }: ICard) => {
+  const { days, people, guides, sleep, difficulty } = details;
+
   return (
     <div className={styles.card}>
       <div className={`${styles.card__side} ${styles.card__side___front}`}>
@@ -11,16 +26,16 @@ const Card = () => {
           <span
             className={`${styles.card__heading__span} ${styles.card__heading__span___1}`}
           >
-            The Sea Explorer
+            {title}
           </span>
         </h4>
         <div className={styles.card__details}>
           <ul>
-            <li>3 day tours</li>
-            <li>Up to 30 people</li>
-            <li>2 tour guides</li>
-            <li>Sleep in cozy hotels</li>
-            <li>Difficulty: easy</li>
+            <li>{days} days tour</li>
+            <li>Up to {people} people</li>
+            <li>{guides} tour guides</li>
+            <li>Sleep in {sleep}</li>
+            <li>Difficulty: {difficulty}</li>
           </ul>
         </div>
       </div>
@@ -31,10 +46,10 @@ const Card = () => {
         <div className={styles.card__cta}>
           <div className={styles.card__price___box}>
             <p className={styles.card__price___only}>Only</p>
-            <p className={styles.card__price___value}>$297</p>
+            <p className={styles.card__price___value}>${price}</p>
           </div>
           <a href="#popup" className="btn btn--white">
-            Book now!
+            {buttonText}
           </a>
         </div>
       </div>
