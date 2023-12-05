614df3b1fa57c2cc687127c683b68192ff6888c4
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-12-05 12:45:18 -0300

    refactor: :recycle: Created Navigation component.

--- src/App.tsx
+++ src/App.tsx
@@ -8,54 +8,12 @@ import logo_green_2x from "@images/logo-green-2x.png";
 
 import "@styles/main.scss";
 import styles from "./App.module.scss";
+import Navigation from "@components/elements/Navigation";
 
 function App() {
   return (
     <div className={styles.app}>
-      <div className="navigation">
-        <input
-          type="checkbox"
-          className="navigation__checkbox"
-          id="navi-toggle"
-        />
-
-        <label htmlFor="navi-toggle" className="navigation__button">
-          <span className="navigation__icon">&nbsp;</span>
-        </label>
-
-        <div className="navigation__background">&nbsp;</div>
-
-        <nav className="navigation__nav">
-          <ul className="navigation__list">
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>01</span>About Natous
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>02</span>Your benfits
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>03</span>Popular tours
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>04</span>Stories
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>05</span>Book now
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
+      <Navigation />
       <header className="header">
         <div className="header__logo-box">
           <img src={logo_white} alt="Logo" className="header__logo" />
---
+++ src/components/elements/Navigation/Navigation.tsx
@@ -0,0 +1,51 @@
+import styles from "./Navigation.module.scss";
+
+const Navigation = () => {
+  return (
+    <div className={styles.navigation}>
+      <input
+        type="checkbox"
+        className="navigation__checkbox"
+        id="navi-toggle"
+      />
+
+      <label htmlFor="navi-toggle" className="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
+
+      <div className="navigation__background">&nbsp;</div>
+
+      <nav className="navigation__nav">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>01</span>About Natous
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>02</span>Your benfits
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>03</span>Popular tours
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>04</span>Stories
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>05</span>Book now
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
